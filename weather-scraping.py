dbc42c4c7db6c9ca28f0582ae06e382548379cf3
Gloria Pappalardo <60324395+grpappalardo@users.noreply.github.com>
2020-09-17 14:08:50 -0400

    Update weather-scraping.py

--- weather-scraping.py
+++ weather-scraping.py
@@ -1,11 +1,21 @@
+'''
+Name: Gloria Pappalardo
+Date: 9/17/20
+Version of Python: 3.6
+Description: This script scrapes information from the National Weather Service 
+website about weather in Montauk, New York. Inputs are longitude and latitude and 
+outputs are weather conditions (wind speed, humidity, barometer, dewpoint,
+visibility, and date and time of last update).
+'''
+
 # Import required libraries
 import requests
 from bs4 import BeautifulSoup
 
 # Provide the latitude and longitude for the location you would like to check the weather for
-# Lat/lon in decimal degrees provided for Worcester, MA
-lat = '42.2634'
-lon = '-71.8022'
+# Lat/lon in decimal degrees provided for Montauk, New York
+lat = '41.031319'
+lon = '-71.950981'
 
 # Create url for the requested location through string concatenation
 url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
@@ -22,13 +32,13 @@ page = requests.get(url)
 soup=BeautifulSoup(page.content,"html.parser")
 
 # Locate element on page to be scraped
-# This element is located within an id tag called current_conditions-summary
+# This element is located within an id tag called current_conditions_detail
 # find() locates the element in the BeautifulSoup object
-cur_weather_conditions = soup.find(id="current_conditions-summary")
+cur_conditions_detail = soup.find(id="current_conditions_detail") # edited from here forward to scrape weather details
 
 # Extract text from the selected BeautifulSoup object using .text
-cur_weather_conditions = cur_weather_conditions.text
+cur_conditions_detail = cur_conditions_detail.text
 
 # Final Output
 # Return scraped information
-print('The Current Weather Conditions at '+ lat +  ", " + lon + " is:" + cur_weather_conditions)
+print('The Current Weather Conditions at '+ lat +  ", " + lon + " are:" + cur_conditions_detail) 
